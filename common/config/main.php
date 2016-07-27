75202eee0285a46776e4bd3ebb02c18be95ee240
ruslan <ruslanerv@gmail.com>
2016-07-27 12:01:10 +0300

    Initial commit

---
+++ common/config/main.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+		
+	    'urlManager' => [
+        'class' => 'yii\web\UrlManager',
+        'enablePrettyUrl' => true,
+        'showScriptName' => false,
+        ],
+
+    ],
+];
